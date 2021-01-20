16aa7520fdcabd73097abb4419207bfe371ea599
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-20 23:33:23 +0100

    initial values ​​established

---
+++ main.js
@@ -0,0 +1,10 @@
+let sequnce = [];
+let humanSequence = [];
+let level = 0;
+
+const startButton = document.querySelector(".js-start");
+const resetButton = document.querySelector(".js-reset");
+const info = document.querySelector(".js-info");
+const heading = document.querySelector(".js-heading");
+const tileContainer = document.querySelector(".js-container");
+const playerTileContainer = document.querySelector(".js-player-container"); 
