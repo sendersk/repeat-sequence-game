2eb0963368686dfc976de64c3464dd14e7497155
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-21 19:08:33 +0100

    resetGame function created

--- main.js
+++ main.js
@@ -7,4 +7,16 @@ const resetButton = document.querySelector(".js-reset");
 const info = document.querySelector(".js-info");
 const heading = document.querySelector(".js-heading");
 const tileContainer = document.querySelector(".js-container");
-const playerTileContainer = document.querySelector(".js-player-container"); 
+const playerTileContainer = document.querySelector(".js-player-container");
+
+function resetGame() {
+  resetButton.classList.add("hidden");
+  sequence = [];
+  humanSequence = [];
+  level = 0;
+  startButton.classList.remove("hidden");
+  heading.textContent = "Repeat Game";
+  info.classList.add("hidden");
+  tileContainer.classList.add("unclickable");
+  playerTileContainer.classList.add("unclickable");
+}
